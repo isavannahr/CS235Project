5645b1655137e722645aea69565539b8faa6d988
Claudia Andrade <candr006@ucr.edu>
2017-12-05 03:46:10 +0000

    added main and association files

---
+++ association.py
@@ -0,0 +1,3 @@
+def main_association():
+  print("Association code goes here");
+
---
+++ main.py
@@ -0,0 +1,21 @@
+/*include files that include our individual functions here*/
+
+ans=True
+while ans:
+    print ("""
+    1.Association
+    2.Clustering
+    3.Sequential Patterns
+    4.Exit/Quit
+    """)
+    ans=raw_input("Choose a Data Mining Technique") 
+    if ans=="1": 
+        /*name of Claudia's function goes here*/
+    elif ans=="2":
+      print("\n Student Deleted") 
+    elif ans=="3":
+      print("\n Student Record Found") 
+    elif ans=="4":
+      print("\n Goodbye") 
+    elif ans !="":
+      print("\n Not Valid Choice Try again") 
