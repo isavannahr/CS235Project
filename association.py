c5676398107686bcdbb08090fd0d67e9b13d8d51
Claudia Andrade <candr006@ucr.edu>
2017-12-11 04:21:12 +0000

    support output

--- association.py
+++ association.py
@@ -1,3 +1,64 @@
 def main_association():
-  print("Association code goes here");
+  #open mass shootings data set and create a dictionary
+  filename_ms = "Datasets/mass_shootings_2014-2017.csv";
+  file_ms = open(filename_ms, "r");
+  array_ms={}
+  count=1;
+  for line in file_ms:
+    if count>1:
+      line_arr=line.split(',')
+      index=line_arr[1]+'-'+line_arr[0]
+      #print index
+      array_ms[index]=line_arr[2]
+    count+=1
 
+#  for i,v in array_ms.items():
+#    print i+'-'+v
+
+  filename_gl = "Datasets/num_gun_laws_by_state_per_year_2014-2017.csv";
+  file_gl = open(filename_gl, "r");
+  header_gl=['ammunition_regulations','assault_weapons_regulations','background_checks','buyer_regulations','child_access_prevention','concealed_carry_permitting','dealer_regulations','domestic_violence','gun_trafficking','immunity','no_stand_your_ground','possession_regulations','preemption','prohibitions_against_high_risk_gun_owners','lawtotal']
+  array_gl={}
+  count=1;
+  array_sums={}
+  for line in file_gl:
+    if count>1:
+      line_arr=line.split(',')
+      index=line_arr[0]+'-'+line_arr[1]
+      temp_map={}
+      array_sums[index]=0
+      count2=2;
+      for h in header_gl:
+        temp_map[h]=line_arr[count2]
+        #array_sums[index]+=temp_map[h]
+        count2+=1
+      temp_map['mass_shootings']=array_ms[index]
+      array_gl[index]=temp_map
+      #array_sums[index]+=temp_map['mass_shootings']
+     
+    count+=1
+  #i= year-state
+  #j= gun law type
+  array_support={}
+  for i,v in array_gl.items():
+     array_support[i]={}
+     for j,x in v.items():
+       if j!='lawtotal':
+         y=float(array_gl[i]['mass_shootings'])
+         if y>0:
+           array_support[i][j]=(float(x)/y)
+         elif y<1:
+           array_support[i][j]='N/A'
+         elif x<1:
+           array_support[i][j]=0
+         else:
+           array_support[i][j]=(float(x)/float(y))
+       
+
+  f = open('association_output.csv','w')
+  f.write('year,state,gun_law_type,support\n');
+  for i,v in array_support.items():
+    for j,x in v.items():
+      line_arr=i.split('-')
+      f.write(str(line_arr[0])+','+str(line_arr[1])+','+str(j)+','+str(x)+'\n')
+         
