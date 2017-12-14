5bfbf700e1f3316d7e08ccdb5c8de06d47c6ed11
Claudia Andrade <candr006@ucr.edu>
2017-12-14 06:34:45 +0000

    run association

--- main.py
+++ main.py
@@ -3,29 +3,5 @@ import sys;
 #from association.py import function main_association
 from association import main_association
 
-ans=True
-while ans:
-    print ("""
-    1.Association
-    2.Clustering
-    3.Outlier Detection
-    4.Sequential Patterns
-    5.Exit/Quit
-    """)
-    ans=raw_input("Choose a Data Mining Technique: ") 
-    if ans=="1": 
-        #call to Claudia's function for Association goes here
-         main_association();
-    elif ans=="2":
-        #call to function
-         print("function2");
-    elif ans=="3":
-       #call to function
-        print("function3");
-    elif ans=="4":
-       #call to function
-        print("function4");
-    elif ans=="5":
-       sys.exit();
-    elif ans !="":
-      print("\n Please select an option from 1-5") 
+main_association();
+
